28abfc62be4587dedba46e075aaf88da23f1de25
Davide Lapi <davide_1502@live.it>
2020-08-04 17:24:56 +0200

    ExampleOfGarbageGame

---
+++ src/main/java/m2/s20ex/Warrior.java
@@ -0,0 +1,14 @@
+package m2.s20ex;
+
+public class Warrior extends Actor {
+	public Warrior(String name) {
+		super(name);
+	}
+
+	public boolean fight(Actor enemy) {
+		if(enemy instanceof Warrior) {
+			return false;             // My Warrior is a monkey
+		}
+		return true;
+	}
+}
